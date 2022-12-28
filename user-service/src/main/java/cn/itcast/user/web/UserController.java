26987050bc70797dc97e6c368ecb3ce01000dcb4
Aliez <17606601564@163.com>
2022-12-28 21:18:00 +0800

    应用了nacos,搭载 nginx

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,12 +1,20 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternPropertiese;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Slf4j
+//@RefreshScope
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -14,6 +22,20 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PatternPropertiese properties;
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
+    }
+
+    @GetMapping("prop")
+    public PatternPropertiese propertiese(){
+        return properties;
+    }
     /**
      * 路径： /user/110
      *
